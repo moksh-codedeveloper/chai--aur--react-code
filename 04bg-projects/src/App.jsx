fda965ca5ba4f3103bf50391f46ba7da91ceb0fb
MokshMalde <maldemoksh437@gmail.com>
2024-05-23 18:16:32 +0530

    new changes

--- 04bg-projects/src/App.jsx
+++ 04bg-projects/src/App.jsx
@@ -4,8 +4,37 @@ function App() {
   const [color, setColor] = useState("olive");
 
   return (
-    <div className="w-full">
-
+    <div className="w-full h-screen "
+      style={{backgroundColor: color}}
+    >
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+          <button
+          onClick={() => {
+            setColor("red");
+          }} 
+          className="ouline-none px-4 py-1 
+          rounded-full text-white shadow-sm"
+          style={{backgroundColor: "red"}}
+          >Red</button>
+          <button
+          onClick={() => {
+            setColor("black");
+          }} 
+          className="ouline-none px-4 py-1 
+          rounded-full text-white shadow-sm"
+          style={{backgroundColor: "black"}}
+          >Black</button>
+          <button
+          onClick={() => {
+            setColor("white");
+          }} 
+          className="ouline-none px-4 py-1 
+          rounded-full text-white shadow-sm"
+          style={{backgroundColor: "lavender"}}
+          >White</button>
+        </div>
+      </div>
     </div>
   )
 }
