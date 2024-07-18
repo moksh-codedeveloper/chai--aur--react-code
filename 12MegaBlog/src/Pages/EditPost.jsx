e70314ea51fc17679b96cb401f44e10d66a98698
MokshMalde <maldemoksh437@gmail.com>
2024-07-18 19:27:27 +0530

    pages and other components for project

---
+++ 12MegaBlog/src/Pages/EditPost.jsx
@@ -0,0 +1,29 @@
+import React,{useState, useEffect} from 'react'
+import {Container, PostForm} from '../components';
+import appwriteService from '../appwrite/conf'
+import { useNavigate, useParams } from 'react-router-dom';
+
+function EditPost() {
+    const [posts, setPosts] = useState([]);
+    const {slug} = useParams();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+       if(slug){ 
+            appwriteService.getPost(slug).then((post) => {
+                if(post) setPosts(post);
+            })
+        } else {
+            navigate('/');
+        }
+    }, [slug, navigate])
+  return posts ? (
+    <div className='py-8'>
+        <Container>
+            <PostForm post={posts}/>
+        </Container>    
+    </div>
+  ) : null;
+}
+
+export default EditPost
