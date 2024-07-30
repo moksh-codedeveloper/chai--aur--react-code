62c278d3aa016a4e5332e85b9a44d6d8bfd10f6d
MokshMalde <maldemoksh437@gmail.com>
2024-07-30 18:37:52 +0530

    delete the folder

--- 12MegaBlog/src/config/config.js
+++
@@ -1,9 +0,0 @@
-const conf = {
-  appWriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
-  appWriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
-  appWriteDataBaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
-  appWriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
-  appWriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
-};
-//this is a better and production grade approach to access the env's and it is much easier and better approach 
-export default conf;
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # chai--aur--react
+
+
+# Todo for Appwrite Blog app 
